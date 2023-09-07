af4070d530d81326cdca7170d01ca6e336638573
Tom Heaton <thomas.m.heaton@gmail.com>
2023-09-07 14:42:26 +0100

    fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::api::process::Command;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +10,11 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+  Command::new_sidecar("server")
+    .expect("failed to create `server` binary command")
+    .spawn()
+    .expect("Failed to spawn sidecar");
+
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import { Command } from "@tauri-apps/api/shell";
+import { useState } from "react";
+
 import { api } from "../utils/api";
 
 export default function Index() {
   const { data } = api.hello.useQuery({ name: "tomheaton" });
+
+  const [message, setMessage] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const handleFetch = async () => {
+    setMessage("");
+    setError("");
+    try {
+      const res = await fetch("http://localhost:1421/trpc");
+      const json = await res.json();
+      setMessage(JSON.stringify(json, null, 2));
+    } catch (e: any) {
+      setError(e.toString());
+      // help me
+    }
+  };
+
+  const handleStartServer = async () => {
+    setMessage("");
+    setError("");
+    try {
+      const command = Command.sidecar("binaries/server");
+      const output = await command.execute();
+      setMessage(JSON.stringify(output, null, 2));
+    } catch (e: any) {
+      setError(e.toString());
+    }
+  };
 
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-blue-200">
@@ -10,6 +41,13 @@ export default function Index() {
       </h1>
       <br />
       <p>{data ?? "none"}</p>
+      <br />
+      <button onClick={handleStartServer}>start server</button>
+      <br />
+      <button onClick={handleFetch}>fetch from 1421</button>
+      <br />
+      {error && <p>Error: {error}</p>}
+      {message && <p>Message: {message}</p>}
     </div>
   );
 }
