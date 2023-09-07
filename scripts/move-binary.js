65685c1be5dba61aad668bf60c8153a398631916
Tom Heaton <thomas.m.heaton@gmail.com>
2023-09-07 06:36:58 +0100

    save

--- scripts/move-binary.js
+++ scripts/move-binary.js
@@ -1,11 +1,5 @@
-/**
- * This script is used to rename the binary with the platform specific postfix.
- * When `tauri build` is ran, it looks for the binary name appended with the platform specific postfix.
- */
-
-// TODO: update execa to v8
-import execa from "execa";
-import fs from "fs";
+const execa = require("execa");
+const fs = require("fs");
 
 let extension = "";
 if (process.platform === "win32") {
@@ -13,13 +7,8 @@ if (process.platform === "win32") {
 }
 
 async function main() {
-  // const {stdout} = await execa('echo', ['unicorns']);
-  // const rustInfo = (await execa("rustc", ["-vV"])).stdout;
   const rustInfo = (await execa("rustc", ["-vV"])).stdout;
-  // const rustInfo = (await execaStdout("rustc", ["-vV"])).stdout;
-
   const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
-  // const targetTriple = /host: (\S+)/g.exec(stdout)[1];
   if (!targetTriple) {
     console.error("Failed to determine platform target triple");
   }
